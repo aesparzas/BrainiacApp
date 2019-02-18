f863512f44e1411ebdec5c62aa3deaf664423eb8
Adolfo Esparza <esparzasordoadolfo@gmail.com>
2019-02-18 00:20:37 -0600

    user dao wip

--- app/src/main/java/com/esparperez/brainiacapp/model/dao/UserDao.java
+++ app/src/main/java/com/esparperez/brainiacapp/model/dao/UserDao.java
@@ -1,7 +1,30 @@
 package com.esparperez.brainiacapp.model.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.esparperez.brainiacapp.model.entity.User;
 
 @Dao
 public interface UserDao {
+    @Query("SELECT * FROM User LIMIT 1")
+    User getDefaultUser();
+
+    @Query("SELECT * FROM User")
+    User getAll();
+
+    @Query("SELECT * FROM User WHERE id_user = :id_user")
+    User getById(int id_user);
+
+    @Update
+    void update(User user);
+
+    @Insert
+    void insert(User user);
+
+    @Delete
+    void delete(User user);
 }
